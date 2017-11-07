1d1dd1e8daa147375156ef0febaec8b39ee23250
Cara Lang <caravlang@gmail.com>
2017-11-07 10:42:47 -0500

    up to adding nunjucks

--- app.js
+++ app.js
@@ -7,12 +7,26 @@ app.use(function (req, res, next) {
   next()
 })
 
+app.use('/special', function (req, res, next) {
+  console.log(`you've reached a special area`);
+  next();
+})
+
+app.get('/special/prices', function (req, res, next) {
+  console.log(`you've reached special prices`);
+  res.send(`<h1>special prices</h1>`)
+})
+
 app.get('/', function(req, res, next) {
   res.send('<h1>Welcome to Twitter</h1>')
 })
 
-app.post('/moderism', function (req, res, next) {
-  res.send('you added')
+app.get('/is-anybody-in-here', function(req, res, next) {
+  res.send(`<h1>I'm here</h1>`)
+})
+
+app.post('/modernism', function (req, res, next) {
+  res.send('you added a new item')
 })
 
 app.listen(3000, function () {
