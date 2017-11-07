f8ffa846d93d064341ccc59b04bc38c01ff34c9e
kat ballo <katballo@icloud.com>
2017-11-07 14:59:22 -0500

    completed live test

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,9 @@ router.get('/', function (req, res) {
   res.render('index', { tweets: tweets } )
 })
 
+router.get('/stylesheets/style.css', function(req, res, next){
+	res.sendFile('/Users/katballo/Desktop/Foundations/twitter-js/public/stylesheets/style.css')
+})
+
+
 module.exports = router;
